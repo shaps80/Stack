a4932573db2ba2cf227f8bf72e7ffaf69d6f5e62
Shaps <shapsuk@me.com>
2016-04-09 16:32:40 +0100

    Fixed schemes so tests will pass

---
+++ Example/Pods/Target Support Files/Pods-StackOSXDemo/Pods-StackOSXDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StackOSXDemo : NSObject
+@end
+@implementation PodsDummy_Pods_StackOSXDemo
+@end
---
+++ Example/Pods/Target Support Files/Pods-StackiOSDemo/Pods-StackiOSDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StackiOSDemo : NSObject
+@end
+@implementation PodsDummy_Pods_StackiOSDemo
+@end
